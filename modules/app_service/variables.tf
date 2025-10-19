abad431a207e69539af48820b77f499169703f90
Srinivas Bobbili <102221196+srinivas9753@users.noreply.github.com>
2025-10-20 01:27:55 +0530

    Add variables for app service configuration

--- modules/app_service/variables.tf
+++ modules/app_service/variables.tf
@@ -1,1 +1,7 @@
-
+variable "app_name" { type = string }
+variable "resource_group_name" { type = string }
+variable "location" { type = string }
+variable "app_service_plan_id" { type = string }
+variable "always_on" { type = bool, default = true }
+variable "app_settings" { type = map(string), default = {} }
+variable "tags" { type = map(string), default = {} }
