f698951dfd17f2dcdc38285292710ed91f119d7f
Srinivas Bobbili <102221196+srinivas9753@users.noreply.github.com>
2025-10-19 22:22:37 +0530

    Update provider versions in provider.tf

--- Terraform/example_root/provider.tf
+++ Terraform/example_root/provider.tf
@@ -1,14 +1,17 @@
 #provider creation
 terraform {
+  required_version = ">= 1.6.0"
+
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "4.45.1"
+      source  = "hashicorp/azurerm"
+      version = "~> 3.120.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5.0"
     }
   }
-}
-provider "azurerm" {
-  features {}
 
 # (Optional) Set default subscription or tenant IDs if not using environment variables
   subscription_id = var.subscription_id
